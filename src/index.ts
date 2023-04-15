6cd5cd98a63f68e077215909cb84159699a41b11
Ravi Teja Darapu <ravievolution87@gmail.com>
2023-04-15 20:16:27 +0530

    updated node typescript app

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-const message = "Hello from Typescript!!";
+import express, { Express, Request, Response } from 'express';
 
-console.log(message);
+const PORT = 3000;
+const app: Express = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Express + TypeScript Server');
+});
+  
+app.listen(PORT, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+});
